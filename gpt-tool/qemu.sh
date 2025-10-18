513e6da65bba0361e88d9b8d9a5f3792cd746c31
zhn2605 <zzihan0805@gmail.com>
2025-10-18 14:57:13 -0400

    enable kvm and pcie qemu

--- gpt-tool/qemu.sh
+++ gpt-tool/qemu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-qemu-system-x86_64 -enable-kvm -machine q35 \
+qemu-system-x86_64 -enable-kvm-machine q35\
 -device ide-hd,drive=disk0,model=NOS\ Boot\ Manager,serial=NOSDISK \
 -drive id=disk0,format=raw,file=test.img,if=none \
 -bios /usr/share/edk2-ovmf/x64/OVMF.4m.fd \
